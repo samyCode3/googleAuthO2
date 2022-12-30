e66c761a6f5367118e211912a79cf4c527f288f5
samyCode3 <107084481+samyCode3@users.noreply.github.com>
2022-12-30 09:18:42 +0100

    First commit
    Google Authorization project

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, {Application, Request, Response, NextFunction} from 'express'
+import env from 'dotenv'
+env.config()
+const app:Application = express()
+const PORT = process.env.PORT
+//MongoDb Db SetUp goes Here
+app.get('/', (req: Request, res: Response, next:NextFunction) => {
+  return res.json('Happy coding')
+})
+//Express App Usage
+app.use(express.json())
+//All Routes goes here
+
+// All Protected Routes goes Here
+app.listen(PORT, () => console.log(`Server running is on ${PORT}`))
